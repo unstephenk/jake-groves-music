10a3bd129a609574080303ab00fdb053d5fe627f
unstephenk <unstephenk@gmail.com>
2018-06-13 17:04:00 -0500

    initial commit

---
+++ js/googlematerialgallery.js
@@ -0,0 +1,9 @@
+/**
+ * Created by skuehl on 8/26/2016.
+ */
+
+var elem = document.querySelector('.m-p-g');
+
+document.addEventListener('DOMContentLoaded', function () {
+    var gallery = new MaterialPhotoGallery(elem);
+});
